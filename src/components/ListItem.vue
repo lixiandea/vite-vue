c597a91f547593edc4be36bea478fa479a368fa7
lixiang.cheng <lixiang.cheng@microfun.com>
2023-12-04 20:39:29 +0800

    init repo

---
+++ src/components/ListItem.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+defineProps<{ name:string, age: number, desc:string }>()
+</script>
+
+<template>
+    <div>
+        <h1>{{ name }}</h1>
+        <p>{{ age }}</p>
+        <p>{{ desc }}</p>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/UserList.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+    import {ListItem} from "@/components/ListItem.vue";
+    defineProps<{userInfoes: {
+            name: string;
+            age: number;
+            desc?: string;
+        }[] }>()
+</script>
+
+
+<template>
+    <div>
+        <div v-for="userInfo in userInfoes">
+            <ListItem :name="userInfo.name" :age="userInfo.age" :desc="userInfo.desc"></ListItem>
+        </div>
+    </div>
+</template>
